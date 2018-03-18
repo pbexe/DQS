adefa167429800b3a1897c0d9c479613b5fb9dba
Miles Budden <miles@budden.net>
2018-03-18 18:17:09 +0000

    Made a start on the questions

--- classes/question.py
+++ classes/question.py
@@ -1,10 +1,55 @@
 class Question:
-    pass
+    def __init__(self):
+        self.__question_text = None
+
+    @property
+    def question_text(self):
+        return self.__question_text
+
+    @question_text.setter
+    def question_text(self, value):
+        self.__question_text = value
 
 
 class Picture_Question(Question):
-    pass
+    def __init__(self):
+        self.__correct_answer = None
+        self.__incorrect_answers = []
+
+    @property
+    def correct_answer(self):
+        return self.__correct_answer
+
+    @correct_answer.setter
+    def correct_answer(self, value):
+        self.__correct_answer = value
+
+    @property
+    def incorrect_answers(self):
+        return self.__incorrect_answers
+
+    @incorrect_answers.setter
+    def incorrect_answers(self, value):
+        self.__incorrect_answers = value
 
 
 class Text_Question(Question):
-    pass
+    def __init__(self):
+        self.__correct_answer = None
+        self.__incorrect_answers = []
+
+    @property
+    def correct_answer(self):
+        return self.__correct_answer
+
+    @correct_answer.setter
+    def correct_answer(self, value):
+        self.__correct_answer = value
+
+    @property
+    def incorrect_answers(self):
+        return self.__incorrect_answers
+
+    @incorrect_answers.setter
+    def incorrect_answers(self, value):
+        self.__incorrect_answers = value
