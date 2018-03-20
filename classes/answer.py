195a1e9c09ffbb8c42d8891bd7df1b055c3564ae
Miles Budden <miles@budden.net>
2018-03-20 19:05:29 +0000

    Added answer classes

--- classes/answer.py
+++ classes/answer.py
@@ -1,10 +1,37 @@
 class Answer:
-    __correct = None
+    def __init__(self, correct):
+        self.__correct = correct
+
+    @property
+    def correct(self):
+        return self.__correct
+
+    @correct.setter
+    def correct(self, value):
+        self.correct = value
 
 
 class Picture_Answer(Answer):
-    pass
+    def __init__(self, correct, image):
+        self.__answer_image = image
+
+    @property
+    def answer_image(self):
+        return self.__answer_image
+
+    @answer_image.setter
+    def answer_image(self, value):
+        self.answer_image = value
 
 
 class Text_Answer(Answer):
-    pass
+    def __init__(self, correct, text):
+        self.__answer_text = text
+
+    @property
+    def answer_text(self):
+        return self.__answer_text
+
+    @answer_text.setter
+    def answer_text(self, value):
+        self.answer_text = value
