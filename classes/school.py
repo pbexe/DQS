94e4594dd21cec18b46de56db5c8232df2ebfe70
Miles Budden <miles@budden.net>
2018-04-14 18:54:22 +0100

    Finished school classes

--- classes/school.py
+++ classes/school.py
@@ -1,10 +1,33 @@
 class School:
-    pass
+    def __init__(self, name, year_group):
+        self.__name = name
+        self.__year_group = year_group
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def year_group(self):
+        return self.__year_group
+
+    @year_group.setter
+    def year_group(self, value):
+        self.__year_group = value
 
 
 class Year_Group:
-    pass
+    def __init__(self, year):
+        self.__year = year
 
+    @property
+    def year(self):
+        return self.__year
 
-class Student:
-    pass
+    @year.setter
+    def year(self, value):
+        self.__year = value
