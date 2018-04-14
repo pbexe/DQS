dfb0ab0fd6f624479116d32ea44a0d15869ea576
Miles Budden <miles@budden.net>
2018-04-14 18:49:01 +0100

    Completed result class

--- classes/result.py
+++ classes/result.py
@@ -1,6 +1,42 @@
 class Result:
-    pass
+    def __init__(self, result, student):
+        self.__result = result
+        self.__student = student
+
+    @property
+    def result(self):
+        return self.__result
+
+    @result.setter
+    def result(self, value):
+        self.__result = value
+
+    @property
+    def student(self):
+        return self.__student
+
+    @student.setter
+    def student(self, value):
+        self.__student = value
 
 
 class Overall_Results:
-    pass
+    def __init__(self, results):
+        self.__results = results
+
+    @property
+    def results(self):
+        return self.__results
+
+    @results.setter
+    def results(self, value):
+        self.__results = value
+
+    def generate_average(self, school, year_group):
+        total = 0
+        sum_ = 0
+        for result in self.__results:
+            if result.student.school == school and result.student.year_group == year_group:
+                total += result.result
+                sum_ += 1
+        return total / sum
